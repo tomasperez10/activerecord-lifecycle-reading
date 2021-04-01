43a3950f57162b657377b46295e6f9b7193c77dd
Tomas Perez <tomasitopg6@gmail.com>
2021-04-01 00:36:01 +0000

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+  before_validation :make_title_case
+
+  before_save :email_author_about_post
 
   private
 
@@ -11,7 +15,11 @@ class Post < ActiveRecord::Base
     end
   end
 
+  def email_author_about_post
+    
+  end
+
   def make_title_case
     self.title = self.title.titlecase
   end
-end
+end 
